4d76e55d20dcf5c6ba9807aadc6e44880022a0b3
maayankoren <nghieuri@gmail.com>
2020-02-10 13:43:43 +0200

    applies samples generator

--- Samples.js
+++ Samples.js
@@ -1,39 +1,31 @@
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Redirect } from 'react-router';
-import FileUploaderView from './FileUploaderView';
-import ImageHandlerView from './ImageHandlerView';
-import CreateGame from './create-game/CreateGame';
-import LoginAsView from './LoginAsView';
-import Login from "../auth/Login";
-import PasswordInputView from "./PasswordInputView"
-
+import loadable from '@loadable/component';
+let samplesArrayData = require("../samples/samples_array_data.json")
+let compName =""
 class Samples extends Component {
+    
     render() {
         return (
             <div>
                 <h3>Pumba's Samples List</h3>
                 <ul dir="ltr">
-                    <li><a href='/samples/file-uploader'>File Uploader, Image Uploader, Audio Uploader</a></li>
-                    <li><a href='/samples/image-handler'>Image Handler (the new image uploader)</a></li>
-                    <li><a href='/samples/create-game'>Upload image to relative model (by creating a new game)</a></li>
-                    <li><a href='/samples/login-as'>Login as another user</a></li>
-                    <li><a href='/samples/login'>Login (Login, reset password, registration (not ready yet)</a></li>
-                    <li><a href='/samples/password-input'>password Input and verify</a></li>
-
+                    {samplesArrayData.map(elem => {
+                        return <li><a href={`/samples/${elem.route}`}>{elem.title}</a></li>
+                    })}
                 </ul>
-
                 <Router basename="samples">
-                    <Switch>
-                        <Route path='/file-uploader' component={FileUploaderView} />
-                        <Route path='/image-handler' component={ImageHandlerView} />
-                        <Route path='/create-game' component={CreateGame} />
-                        <Route path='/login-as' component={LoginAsView} />
-                        <Route path='/login' component={Login} />
-                        <Route path='/password-input' component={PasswordInputView} />
+                    {
+                        <Switch>
+                            {samplesArrayData.map(elem => {
+                                let i = elem.filePath;
+                                compName = loadable(() => import("../" + i))
+                                return < Route path={`/${elem.route}`} component={compName} />
+                                
+                            })}
 
-                        <Redirect to="/" />
-                    </Switch>
+                        </Switch>}
                 </Router>
             </div>
         );
